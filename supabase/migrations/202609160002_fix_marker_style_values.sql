aa8b81de6ad9cf2d269474a71e218ecb6ddb4e6b
oopss-develop <woojjing12@gmail.com>
2026-09-16 18:31:47 +0900

    fix: allow current marker styles in database

---
+++ supabase/migrations/202609160002_fix_marker_style_values.sql
@@ -0,0 +1,14 @@
+alter table public.visits
+  alter column marker_style set default 'black-1';
+
+update public.visits
+set marker_style = 'black-1'
+where marker_style is null
+   or marker_style !~ '^(color|black)-[0-9]+$';
+
+alter table public.visits
+  drop constraint if exists visits_marker_style_check;
+
+alter table public.visits
+  add constraint visits_marker_style_check
+  check (marker_style ~ '^(color|black)-[0-9]+$');
